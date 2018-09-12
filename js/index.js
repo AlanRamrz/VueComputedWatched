e30f255c5b13dc00bebca2b2b45dad29b24c1fbd
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-09-11 19:37:50 -0500

    Adding watched property

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var app = new Vue({
-  el: '#app',
+var computedApp = new Vue({
+  el: '#computed',
   data: {
     person: {
       name: "Mini",
@@ -10,6 +10,27 @@ var app = new Vue({
   computed: {
     description: function(){
       return "Hi there, my name is " + this.person.name + " " + this.person.lastName + " and i'm " + this.person.age;
+    }
+  }
+})
+
+var watchedApp = new Vue({
+  el: '#watched',
+  data: {
+    number: 100,
+    answer: 'The number is smaller than 500'
+  },
+  watch: {
+    number: function(newValue){
+      if (newValue > 500){
+        this.answer = 'The number is bigger than 500';
+      }
+      else if(newValue < 500){
+        this.answer = 'The number is smaller than 500';
+      }
+      else{
+        this.answer = 'The number is equal to 500';
+      }
     }
   }
 })
