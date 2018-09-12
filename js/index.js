0fe7f10375ed75c58911a9f9ab391adc5402a8f9
Alan Ramirez <alanramirez@MacBook-Pro-de-Alan.local>
2018-09-11 19:07:30 -0500

    Adding computed property

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hi there!'
+    person: {
+      name: "Mini",
+      lastName: "Coding",
+      age: 26
+    }
+  },
+  computed: {
+    description: function(){
+      return "Hi there, my name is " + this.person.name + " " + this.person.lastName + " and i'm " + this.person.age;
+    }
   }
 })
